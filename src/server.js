fa98afa2d1993f6e1f172ce603323aa171371a7c
Yevhen Voiuiev <vouev.e.s@gmail.com>
2025-10-14 14:01:13 +0300

    Express: env

---
+++ src/server.js
@@ -0,0 +1,70 @@
+// src/index.js
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+export const startServer = () => {
+
+  const app = express();
+
+  app.use(cors(/*corsOptions*/));
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  // Middleware для логування часу запиту
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+
+  // Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+  // наприклад, у запитах POST або PATCH
+  app.use(express.json());
+
+  // Маршрут для обробки GET-запитів на '/'
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello, World!',
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  // Middleware для обробких помилок (приймає 4 аргументи)
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+}
